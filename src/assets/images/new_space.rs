2635de8a97a8d3fa2bef91b7351f164ca85d9d52
Skippy <steelstone3@gmail.com>
2024-05-28 19:14:19 +0100

    update

--- src/assets/images/new_space.rs
+++ src/assets/images/new_space.rs
@@ -3,7 +3,7 @@ use rand_derive2::RandGen;
 use std::fmt::Display;
 
 #[derive(RandGen, Copy, Clone, Debug, PartialEq, Reflect)]
-pub enum NewSpaceSprite {
+pub enum SpaceSprite {
     Space1,
     Space2,
     Space3,
@@ -26,67 +26,67 @@ pub enum NewSpaceSprite {
     Space20,
 }
 
-impl Display for NewSpaceSprite {
+impl Display for SpaceSprite {
     fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            NewSpaceSprite::Space1 => {
+            SpaceSprite::Space1 => {
                 write!(formatter, "images/new_space/space_1.png")
             }
-            NewSpaceSprite::Space2 => {
+            SpaceSprite::Space2 => {
                 write!(formatter, "images/new_space/space_2.png")
             }
-            NewSpaceSprite::Space3 => {
+            SpaceSprite::Space3 => {
                 write!(formatter, "images/new_space/space_3.png")
             }
-            NewSpaceSprite::Space4 => {
+            SpaceSprite::Space4 => {
                 write!(formatter, "images/new_space/space_4.png")
             }
-            NewSpaceSprite::Space5 => {
+            SpaceSprite::Space5 => {
                 write!(formatter, "images/new_space/space_5.png")
             }
-            NewSpaceSprite::Space6 => {
+            SpaceSprite::Space6 => {
                 write!(formatter, "images/new_space/space_6.png")
             }
-            NewSpaceSprite::Space7 => {
+            SpaceSprite::Space7 => {
                 write!(formatter, "images/new_space/space_7.png")
             }
-            NewSpaceSprite::Space8 => {
+            SpaceSprite::Space8 => {
                 write!(formatter, "images/new_space/space_8.png")
             }
-            NewSpaceSprite::Space9 => {
+            SpaceSprite::Space9 => {
                 write!(formatter, "images/new_space/space_9.png")
             }
-            NewSpaceSprite::Space10 => {
+            SpaceSprite::Space10 => {
                 write!(formatter, "images/new_space/space_10.png")
             }
-            NewSpaceSprite::Space11 => {
+            SpaceSprite::Space11 => {
                 write!(formatter, "images/new_space/space_11.png")
             }
-            NewSpaceSprite::Space12 => {
+            SpaceSprite::Space12 => {
                 write!(formatter, "images/new_space/space_12.png")
             }
-            NewSpaceSprite::Space13 => {
+            SpaceSprite::Space13 => {
                 write!(formatter, "images/new_space/space_13.png")
             }
-            NewSpaceSprite::Space14 => {
+            SpaceSprite::Space14 => {
                 write!(formatter, "images/new_space/space_14.png")
             }
-            NewSpaceSprite::Space15 => {
+            SpaceSprite::Space15 => {
                 write!(formatter, "images/new_space/space_15.png")
             }
-            NewSpaceSprite::Space16 => {
+            SpaceSprite::Space16 => {
                 write!(formatter, "images/new_space/space_16.png")
             }
-            NewSpaceSprite::Space17 => {
+            SpaceSprite::Space17 => {
                 write!(formatter, "images/new_space/space_17.png")
             }
-            NewSpaceSprite::Space18 => {
+            SpaceSprite::Space18 => {
                 write!(formatter, "images/new_space/space_18.png")
             }
-            NewSpaceSprite::Space19 => {
+            SpaceSprite::Space19 => {
                 write!(formatter, "images/new_space/space_19.png")
             }
-            NewSpaceSprite::Space20 => {
+            SpaceSprite::Space20 => {
                 write!(formatter, "images/new_space/space_20.png")
             }
         }
