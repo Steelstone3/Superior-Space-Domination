6d8ab87b99f692c0dc5b1a5f55741ce1c3bd805a
Skippy <steelstone3@gmail.com>
2024-05-28 20:17:34 +0100

    update

--- src/plugins/developer.rs
+++ src/plugins/developer.rs
@@ -1,14 +1,7 @@
-use bevy::app::{Plugin, Update};
-
-use crate::{
-    events::event_handlers::spawn_animated_sprite::spawn_animated_sprite,
-    systems::animation::animate::animate_sprites,
-};
+use bevy::app::Plugin;
 
 pub struct DeveloperPlugin;
 
 impl Plugin for DeveloperPlugin {
-    fn build(&self, _app: &mut bevy::prelude::App) {
-        _app.add_systems(Update, (animate_sprites, spawn_animated_sprite));
-    }
+    fn build(&self, _app: &mut bevy::prelude::App) {}
 }
--- src/plugins/running.rs
+++ src/plugins/running.rs
@@ -6,6 +6,7 @@ use bevy::{
 use crate::{
     events::{event_handlers::select::select, spawn_planet_event::SpawnPlanetEvent},
     systems::{
+        animation::animate::animate_sprites,
         camera::{
             camera_movement::camera_movement, camera_position_reset::camera_position_reset,
             camera_zoom_keyboard::camera_zoom_keyboard,
@@ -31,6 +32,7 @@ impl Plugin for RunningPlugin {
                 camera_position_reset,
                 select,
                 select_selectable,
+                animate_sprites,
             ),
         )
         .add_systems(
