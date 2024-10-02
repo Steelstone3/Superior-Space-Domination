2c5d1ffc9f4e6dff42f744901f426f7f6416b983
Skippy <steelstone3@gmail.com>
2024-10-02 19:07:26 +0100

    version-02-10-24

--- src/plugins/groups/user_interface_plugin.rs
+++ src/plugins/groups/user_interface_plugin.rs
@@ -9,6 +9,6 @@ pub struct UserInterfacePlugin;
 impl Plugin for UserInterfacePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_systems(Startup, spawn_menu);
-        app.add_systems(Update, spawn_starship);
+        // app.add_systems(Update, spawn_starship);
     }
 }
