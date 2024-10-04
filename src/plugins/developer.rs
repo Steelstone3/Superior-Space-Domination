9016fa49a75523d5288275f0df059c21a7fc0ccd
Skippy <steelstone3@gmail.com>
2024-10-04 13:43:42 +0100

    version-04-10-24

--- src/plugins/developer.rs
+++ src/plugins/developer.rs
@@ -1,15 +1,9 @@
-use bevy::{
-    app::{Plugin, Startup},
-    prelude::IntoSystemConfigs,
-};
+use bevy::app::Plugin;
 
-use crate::systems::spawning::{
-    new_spawn_starter_ship::spawn_starter_spaceship, spawn_player_base::spawn_space_stations,
-};
 
 pub struct DeveloperPlugin;
 
 impl Plugin for DeveloperPlugin {
-    fn build(&self, app: &mut bevy::prelude::App) {
+    fn build(&self, _app: &mut bevy::prelude::App) {
     }
 }
